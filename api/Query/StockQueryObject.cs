5b8446f64eff43ccb08db94de46e41622f074224
kamel-mac <kamel12arag@gmail.com>
2024-11-29 20:12:18 +0200

    Sorting -> DONE

--- api/Query/StockQueryObject.cs
+++ api/Query/StockQueryObject.cs
@@ -4,4 +4,6 @@ public class StockQueryObject
 {
     public String CompanyName { get; set; } = String.Empty;
     public String Symbol { get; set; } = String.Empty;
+    public String? SortBy { get; set; } = null;
+    public bool IsDecsending { get; set; } = false;
 }
