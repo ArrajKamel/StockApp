e93829c879d91198d33ffa2821facdf44f6fcd3e
kamel-mac <kamel12arag@gmail.com>
2024-11-29 20:36:32 +0200

    Pagination -> DONE

--- api/Query/StockQueryObject.cs
+++ api/Query/StockQueryObject.cs
@@ -6,4 +6,6 @@ public class StockQueryObject
     public String Symbol { get; set; } = String.Empty;
     public String? SortBy { get; set; } = null;
     public bool IsDecsending { get; set; } = false;
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
 }
